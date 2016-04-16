82db517424f2503c7a1318f61a1587aeef8a5c54
Michael Lynch <mtlynch@google.com>
2016-04-16 12:43:42 -0400

    Adding clock, unit tests for pump
    Adds an implementation for a clock interface.
    Adds unit tests and a stub for the pump_water method of Pump class.

---
+++ greenpithumb/adc.py
@@ -0,0 +1,41 @@
+
+# TODO(mtlynch): Move these pin constants to a configuration file so they're not
+# hardcoded in.
+
+# ADC pin to which water pump is connected
+PIN_PUMP = 0
+
+# Signal to turn an ADC pin off.
+SIGNAL_OFF = 0
+
+# Signal to turn an ADC pin on.
+SIGNAL_ON = 1023
+
+
+class Adc(object):
+    """Wrapper for an MCP3008 analog to digital converter (ADC).
+
+    This wraps the ADC and allows the caller to read or send digital signals to
+    the ADC's eight I/O pins.
+    """
+
+    def read_pin(self, pin):
+        """Reads a digital value from an ADC pin.
+
+        Args:
+            pin: Index of ADC pin to read from (must be an int between 0 and 7).
+
+        Returns:
+            The signal read from the ADC pin (an int between 0 and 1023).
+        """
+        raise NotImplementedError()
+
+    def write_pin(self, pin, value):
+        """Writes a digital value to an ADC pin.
+
+        Args:
+            pin: Index of ADC pin to write to (must be an int between 0 and 7).
+            value: Value to write to the pin (must be an int between 0 and
+                1023).
+        """
+        raise NotImplementedError()
