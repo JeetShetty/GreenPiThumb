03575a553e7c8c387fe21ad97b948d95bc8b65d6
JeetShetty <jeetorious@gmail.com>
2016-07-21 14:51:46 -0400

    Add sqlite tables. (#26)
    * Add sqlite tables.

---
+++ greenpithumb/sql/create_tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE temperature
+(
+    timestamp TEXT
+    temperature REAL    --ambient temperature (in degrees Celsius)
+);
+
+CREATE TABLE ambient_humidity
+(
+    timestamp TEXT
+    humidity REAL
+);
+
+CREATE TABLE soil_moisture
+(
+    timestamp TEXT
+    soil_moisture REAL
+);
+
+CREATE TABLE ambient_light
+(
+    timestamp TEXT
+    light REAL
+);
+
+CREATE TABLE reservoir_level
+(
+    timestamp TEXT
+    level REAL  -- reservoir level (in mL)
+);
+
+CREATE TABLE watering_events
+(
+    timestamp TEXT
+    water_pumped REAL   --amount of water pumped (in mL)
+);
