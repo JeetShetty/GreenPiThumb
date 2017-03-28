a4c037958cb092233f2a6c2ab893cae407c26d48
JeetShetty <jeetorious@gmail.com>
2017-03-28 08:08:59 -0400

    Fix PumpScheduler to handle updated sleep window format. (#92)

--- greenpithumb/pump_scheduler.py
+++ greenpithumb/pump_scheduler.py
@@ -1,27 +1,32 @@
 class PumpScheduler(object):
     """A wrapper for managing pump scheduling."""
 
-    def __init__(self, clock, sleep_windows):
+    def __init__(self, local_clock, sleep_windows):
         """Creates new PumpScheduler wrapper.
 
         Args:
-            clock: A clock interface
-            sleep_windows: A tuple of tuples, each consisting of a sleep
-                window. Time zone should match that of the clock interface.
+            local_clock: A local clock interface
+            sleep_windows: A list of 2-tuples, each representing a sleep window.
+                Tuple items are datetime.time objects.
         """
-        self._clock = clock
+        self._local_clock = local_clock
         self._sleep_windows = sleep_windows
 
     def is_running_pump_allowed(self):
-        """Returns True if OK to run pump, otherwise False."""
-        current_hour = self._clock.now().hour
+        """Returns True if OK to run pump, otherwise False.
 
-        for sleep_hour, wake_hour in self._sleep_windows:
-            if wake_hour < sleep_hour:
-                if current_hour >= sleep_hour or current_hour < wake_hour:
+        Pump is not allowed to run from the start of a sleep window (inclusive)
+        to the end of a sleep window (exclusive).
+        """
+        current_time = self._local_clock.now().time()
+
+        for sleep_time, wake_time in self._sleep_windows:
+            # Check if sleep window wraps midnight.
+            if wake_time < sleep_time:
+                if current_time >= sleep_time or current_time < wake_time:
                     return False
             else:
-                if sleep_hour <= current_hour < wake_hour:
+                if sleep_time <= current_time < wake_time:
                     return False
 
         return True
--- tests/test_pump_scheduler.py
+++ tests/test_pump_scheduler.py
@@ -10,60 +10,63 @@ from greenpithumb import pump_scheduler
 class PumpSchedulerTest(unittest.TestCase):
 
     def setUp(self):
-        self.mock_clock = mock.Mock()
+        self.mock_local_clock = mock.Mock()
 
-    def test_wake_hour_less_than_sleep_hour(self):
+    def test_wake_time_less_than_sleep_time(self):
         """Test rollover from one day to the next.
 
         If the sleep period spans two days, the pump should not run between
         midnight and the wake hour.
         """
-        self.mock_clock.now.return_value = datetime.datetime(
-            2016, 5, 24, 0, tzinfo=pytz.utc)
-        sleep_windows = [(22, 8)]
-        pump_sched = pump_scheduler.PumpScheduler(self.mock_clock,
+        self.mock_local_clock.now.return_value = (datetime.datetime(
+            2016, 7, 23, 10, 51, 9, 928000, tzinfo=pytz.utc))
+        sleep_windows = [(datetime.time(23, 0), datetime.time(11, 0))]
+        pump_sched = pump_scheduler.PumpScheduler(self.mock_local_clock,
                                                   sleep_windows)
         self.assertFalse(pump_sched.is_running_pump_allowed())
 
-    def test_wake_hour_greater_than_sleep_hour(self):
-        self.mock_clock.now.return_value = datetime.datetime(
-            2016, 5, 24, 4, tzinfo=pytz.utc)
-        sleep_windows = [(2, 8)]
-        pump_sched = pump_scheduler.PumpScheduler(self.mock_clock,
+    def test_wake_time_greater_than_sleep_time(self):
+        self.mock_local_clock.now.return_value = (datetime.datetime(
+            2016, 7, 23, 10, 51, 9, 928000, tzinfo=pytz.utc))
+        sleep_windows = [(datetime.time(5, 23), datetime.time(14, 15))]
+        pump_sched = pump_scheduler.PumpScheduler(self.mock_local_clock,
                                                   sleep_windows)
         self.assertFalse(pump_sched.is_running_pump_allowed())
 
     def test_multiple_sleep_windows(self):
         """Pump should not run if current hour is in any sleep window."""
-        self.mock_clock.now.return_value = datetime.datetime(
-            2016, 5, 24, 4, tzinfo=pytz.utc)
-        sleep_windows = [(16, 20), (2, 8)]
-        pump_sched = pump_scheduler.PumpScheduler(self.mock_clock,
+        self.mock_local_clock.now.return_value = (datetime.datetime(
+            2016, 7, 23, 10, 51, 9, 928000, tzinfo=pytz.utc))
+        sleep_windows = [(datetime.time(16, 30), datetime.time(20, 45)),
+                         (datetime.time(8, 13), datetime.time(11, 18))]
+        pump_sched = pump_scheduler.PumpScheduler(self.mock_local_clock,
                                                   sleep_windows)
         self.assertFalse(pump_sched.is_running_pump_allowed())
 
     def test_running_pump_allowed_if_not_in_sleep_window(self):
-        self.mock_clock.now.return_value = datetime.datetime(
-            2016, 5, 24, 10, tzinfo=pytz.utc)
-        sleep_windows = [(2, 8)]
-        pump_sched = pump_scheduler.PumpScheduler(self.mock_clock,
+        self.mock_local_clock.now.return_value = (datetime.datetime(
+            2016, 7, 23, 10, 51, 9, 928000, tzinfo=pytz.utc))
+        sleep_windows = [(datetime.time(2, 11), datetime.time(8, 33))]
+        pump_sched = pump_scheduler.PumpScheduler(self.mock_local_clock,
                                                   sleep_windows)
         self.assertTrue(pump_sched.is_running_pump_allowed())
 
-    def test_current_hour_equal_to_sleep_hour(self):
+    def test_current_hour_and_minute_equal_to_sleep_time(self):
         """Running pump should not be allowed."""
-        self.mock_clock.now.return_value = datetime.datetime(
-            2016, 5, 24, 2, tzinfo=pytz.utc)
-        sleep_windows = [(2, 8)]
-        pump_sched = pump_scheduler.PumpScheduler(self.mock_clock,
+        now = datetime.datetime(2016, 7, 23, 10, 51, 9, 928000, tzinfo=pytz.utc)
+        self.mock_local_clock.now.return_value = now
+        sleep_windows = [(datetime.time(now.hour, now.minute),
+                          datetime.time(11, 0))]
+        pump_sched = pump_scheduler.PumpScheduler(self.mock_local_clock,
                                                   sleep_windows)
         self.assertFalse(pump_sched.is_running_pump_allowed())
 
-    def test_current_hour_equal_to_wake_hour(self):
+    def test_current_hour_and_minute_equal_to_wake_time(self):
         """Running pump should be allowed."""
-        self.mock_clock.now.return_value = datetime.datetime(
-            2016, 5, 24, 8, tzinfo=pytz.utc)
-        sleep_windows = [(2, 8)]
-        pump_sched = pump_scheduler.PumpScheduler(self.mock_clock,
+        now = datetime.datetime(2016, 7, 23, 10, 51, 9, 928000, tzinfo=pytz.utc)
+        self.mock_local_clock.now.return_value = now
+        sleep_windows = [(datetime.time(10, 0), datetime.time(now.hour,
+                                                              now.minute))]
+        pump_sched = pump_scheduler.PumpScheduler(self.mock_local_clock,
                                                   sleep_windows)
         self.assertTrue(pump_sched.is_running_pump_allowed())
